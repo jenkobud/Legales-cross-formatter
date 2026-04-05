0b4737448041b062619caa6d16d272b23638153b
Jenko E. Budnechky <jbudnechky@fi.uba.ar>
2026-04-04 21:40:42 -0300

    subo cambios x formateo.

--- src/main/java/org/daos/JSONRetrieverDao.java
+++ src/main/java/org/daos/JSONRetrieverDao.java
@@ -2,5 +2,12 @@ package org.daos;
 
 public interface JSONRetrieverDao {
 
+    /**
+     * Get the legal object from the JSON file
+     * @param legalType
+     * @param campaign
+     * @param clazz
+     * @return
+     */
     Object getLegalGeneric(String legalType, String campaign, Class<?> clazz);
 }
