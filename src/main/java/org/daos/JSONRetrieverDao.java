2ffb75122ca5919c0b1aa76603357811a0a8a5c8
Jenko E. Budnechky <jbudnechky@fi.uba.ar>
2026-04-04 21:42:06 -0300

    Revert "subo cambios x formateo."
    This reverts commit 0b4737448041b062619caa6d16d272b23638153b.

--- src/main/java/org/daos/JSONRetrieverDao.java
+++ src/main/java/org/daos/JSONRetrieverDao.java
@@ -2,12 +2,5 @@ package org.daos;
 
 public interface JSONRetrieverDao {
 
-    /**
-     * Get the legal object from the JSON file
-     * @param legalType
-     * @param campaign
-     * @param clazz
-     * @return
-     */
     Object getLegalGeneric(String legalType, String campaign, Class<?> clazz);
 }
